9b2d39025d821ef08baa9ac1b19337b5010dc635
Ygor-Andrade <111753698+Ygor-Andrade@users.noreply.github.com>
2022-10-19 08:45:11 -0300

    update

---
+++ processa.php
@@ -0,0 +1,28 @@
+<?php 
+ 
+// phpinfo(); 
+// Recebe os campos dos formulário
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$sexo = $_POST['sexo'];
+$telefone = $_POST['telefone'];
+$senha  = $_POST['senha'];
+$idade  = $_POST['idade'];
+$estado = $_POST['estado'];
+$cidade = $_POST['cidade'];
+$destinos = $_POST['destinos'];
+$hospedagens = $_POST['hospedagem']; 
+$mensagem = $_POST['mensagem'];
+$dt_cadastro = $_POST['dt-cadastro'];
+
+
+//Para investigar 
+// var_dump($_POST);
+
+
+
+
+
+
+
